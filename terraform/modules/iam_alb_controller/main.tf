6005ac3e292f02e386b1b1c48c5d2ff964a0e085
csm123455 <73813506+csm123455@users.noreply.github.com>
2025-07-12 16:36:02 +0900

    oidc 공급자 존재 에러
    data "aws_iam_openid_connect_provider" "oidc" {
      url = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
    }
    
     identifiers = [data.aws_iam_openid_connect_provider.oidc.arn]
     variable = "${replace(data.aws_iam_openid_connect_provider.oidc.url, "https://", "")}:sub"
    추가

--- terraform/modules/iam_alb_controller/main.tf
+++ terraform/modules/iam_alb_controller/main.tf
@@ -6,26 +6,30 @@ data "aws_eks_cluster_auth" "cluster" {
   name = var.cluster_name
 }
 
-resource "aws_iam_openid_connect_provider" "oidc" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da0ecd4e9b5"] # AWS 기본 thumbprint
-  url             = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+#resource "aws_iam_openid_connect_provider" "oidc" {
+#  client_id_list  = ["sts.amazonaws.com"]
+#  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da0ecd4e9b5"] # AWS 기본 thumbprint
+#  url             = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+#}
+data "aws_iam_openid_connect_provider" "oidc" {
+  url = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
 }
-
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
 
     principals {
       type        = "Federated"
-      identifiers = [aws_iam_openid_connect_provider.oidc.arn]
+     # identifiers = [aws_iam_openid_connect_provider.oidc.arn]
+      identifiers = [data.aws_iam_openid_connect_provider.oidc.arn]
     }
 
     actions = ["sts:AssumeRoleWithWebIdentity"]
 
     condition {
       test     = "StringEquals"
-      variable = "${replace(aws_iam_openid_connect_provider.oidc.url, "https://", "")}:sub"
+    #  variable = "${replace(aws_iam_openid_connect_provider.oidc.url, "https://", "")}:sub"
+      variable = "${replace(data.aws_iam_openid_connect_provider.oidc.url, "https://", "")}:sub"
       values   = ["system:serviceaccount:kube-system:aws-load-balancer-controller"]
     }
   }
