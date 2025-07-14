ef0c574912b48edf173089e6917b20744c0f8516
JongHoon Kim <ref0009@naver.com>
2025-07-14 11:30:11 +0900

    fix: main.tf 수정

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-  required_version = ">= 1.3"
-}
-
 provider "aws" {
   region = "ap-northeast-2"
 }
